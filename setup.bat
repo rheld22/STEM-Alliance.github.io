8c892ef24119a6e1f46829048f9169f565f219e3
Brad Poppe <poppeseed@gmail.com>
2018-12-25 17:26:42 -0600

    Updating tech resources. Gemfile updates

--- setup.bat
+++ setup.bat
@@ -1,3 +1,9 @@
 @echo off
-gem install bundler
+gem install bundler 
+if %ERRORLEVEL% == 0 goto :next
+echo "Unable to find gem. Install Ruby and relaunch the command prompt"
+start "" https://github.com/oneclick/rubyinstaller2/releases/download/rubyinstaller-2.5.3-1/rubyinstaller-2.5.3-1-x64.exe
+goto :endofscript
+:next
 bundle install
+:endofscript
